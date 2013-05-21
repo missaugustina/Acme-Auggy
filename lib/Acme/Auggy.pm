8bbff5fdc4c1b3578db9b32d00d9d4255bd8b0e4
Augustina Ragwitz <auggy@cpan.org>
2013-05-20 21:41:41 -0400

    Initial version

---
+++ lib/Acme/Auggy.pm
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+package Acme::Auggy;
+
+sub print_auggy {
+    print "Auggy!\n";
+}
+
+1;
+
+# ABSTRACT: Just a module for my talk
+
+=head SYNOPSIS
+
+This module is for demonstration purposes only.
+
+=method print_auggy
+
+Just prints my name ;)
+
